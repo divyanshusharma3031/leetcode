08afdf41a1aa9aaf55ee1041576e4f86419f6c75
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-01-04 23:27:49 +0530

    Time: 49 ms (5.58%), Space: 9.6 MB (6.12%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)
+        {
+            return false;
+        }
+        vector<int> arr;
+        while(x)
+        {
+            arr.push_back(x%10);
+            x=x/10;
+        }
+        int i=0;
+        int j=arr.size()-1;
+        while(i<=j)
+        {
+            if(arr[i]!=arr[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
