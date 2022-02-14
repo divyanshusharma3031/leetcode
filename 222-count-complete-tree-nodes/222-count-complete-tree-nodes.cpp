a51f804695d02e8f5a4592c9f746407722853a01
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-02-14 18:48:35 +0530

    Time: 20 ms (99.27%), Space: 31.6 MB (6.09%) - LeetHub

---
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void leaf(TreeNode *root,vector<TreeNode*> &arr,int h)
+    {
+        if(!root)
+        {
+            return;
+        }
+        if(h==1)
+        {
+            arr.push_back(root);
+            return;
+        }
+        leaf(root->left,arr,h-1);
+        leaf(root->right,arr,h-1);
+    }
+    int countNodes(TreeNode* root) {
+        long long h=0;
+        TreeNode *p=root;
+        while(p)
+        {
+            p=p->left;
+            h++;
+        }
+        if(h==1)
+        {
+            return 1;
+        }
+        p=root;
+        TreeNode *prev=NULL;
+        long long c=pow(2,h-1)-1;
+        vector<TreeNode *> arr;
+        leaf(root,arr,h-1);
+        cout<<arr.size()<<"\n";
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]->left && arr[i]->right)
+            {
+                c+=2;
+            }
+            else if(!arr[i]->left && !arr[i]->right)
+            {
+                //pass
+            }
+            else
+            {
+                c+=1;
+            }
+        }
+        return c;
+    }
+};
