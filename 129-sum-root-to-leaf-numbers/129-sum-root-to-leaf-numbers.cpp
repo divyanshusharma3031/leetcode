11e13584b6fe58d03ba8478fafe47150d6b973bf
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-02-14 19:42:15 +0530

    Time: 4 ms (49.79%), Space: 9.2 MB (81.92%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
@@ -11,26 +11,22 @@
  */
 class Solution {
 public:
-    void sumnum(TreeNode *root,map<TreeNode*,int> &mpp,long long &s,long long curr)
+    void sumnum(TreeNode *root,long long &s,long long curr)
     {
         if(root==NULL)
         {
             return;
         }
-        if(mpp[root]==1)
-        {
-            return;
-        }
         if(root->left==NULL &&root->right==NULL)
         {s+=curr*10+root->val;}
-        mpp[root]=1;
-        sumnum(root->left,mpp,s,curr*10+root->val);
-        sumnum(root->right,mpp,s,curr*10+root->val);
+        // mpp[root]=1;
+        sumnum(root->left,s,curr*10+root->val);
+        sumnum(root->right,s,curr*10+root->val);
     }
     int sumNumbers(TreeNode* root) {
         map<TreeNode *,int> mpp;
         long long s=0;
-        sumnum(root,mpp,s,0LL);
+        sumnum(root,s,0LL);
         return s;
     }
 };
