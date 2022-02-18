b9bf5a74214335809bfb8537933a23464a61e07a
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-02-18 17:13:39 +0530

    Time: 7 ms (6.64%), Space: 5.9 MB (69.46%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<0)
+        {
+            return false;
+        }
+        int x=__builtin_popcount(n);
+        if(x==1)
+        {
+            return true;
+        }
+        return false;
+    }
+};
