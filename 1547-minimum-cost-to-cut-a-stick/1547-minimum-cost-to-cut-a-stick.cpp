c4242cad33035fd2eb537f872136d5ab0b4ee893
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-02-20 22:57:45 +0530

    Time: 56 ms (73.11%), Space: 8.4 MB (74.64%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,52 +1,32 @@
 class Solution {
 public:
-    int mincost(int start,int end,vector<int> cuts,int sizeofrod,vector<vector<int>> &dp)
+    int dp[103][103];
+    int mincost(int start,int end,vector<int> &cuts,int l,int r)
     {
+        if(start>end || end>=cuts.size() || start<0)
+        {
+            return 0;
+        }
+        if(start==end)
+        {
+            return r-l;
+        }
         int cost=INT_MAX;
         int ans=0;
-        // cout<<start<<" "<<end<<"\n";
-        if(dp[start][end]!=0)
+        if((dp[start][end])!=-1)
         {
             return dp[start][end];
         }
-        if(start>=end)
+        for(int i=start;i<=end;i++)
         {
-            return 0;
+            cost=min(cost,r-l+mincost(start,i-1,cuts,l,cuts[i])+mincost(i+1,end,cuts,cuts[i],r));
         }
-        if((end-start)==1)
-        {
-            return 0;
-        }
-        for(int i=start+1;i<end;i++)
-        {
-            // cout<<cuts[end]<<"\n";
-            cost=min(cost,cuts[end]-cuts[start]+mincost(start,i,cuts,cuts[i]-start,dp)+mincost(i,end,cuts,end-cuts[i],dp));
-            dp[start][end]=cost;
-        }
+        dp[start][end]=cost;
         return cost;
     }
-//     int bottomup(int n,vector<int> cuts)
-//     {
-//         vector<vector<int>> dp(n,vector<int>(n));
-//         for(int i=2;i<n;i++)
-//         {
-            
-//         }
-//     }
-    int minCost(int n, vector<int>& A) {
-        A.push_back(0);
-        A.push_back(n);
-        sort(A.begin(), A.end());
-        int k = A.size();
-        vector<vector<int>> dp(k, vector<int>(k));
-        for (int d = 2; d < k; ++d) {
-            for (int i = 0; i < k - d; ++i) {
-                dp[i][i + d] = 1e9;
-                for (int m = i + 1; m < i + d; ++m) {
-                    dp[i][i + d] = min(dp[i][i + d], dp[i][m] + dp[m][i + d] + A[i + d] - A[i]);
-                }
-            }
-        }
-        return dp[0][k - 1];
+    int minCost(int n, vector<int>& cuts) {
+        sort(cuts.begin(),cuts.end());
+        memset(dp,-1,sizeof dp);
+        return mincost(0,cuts.size()-1,cuts,0,n);
     }
 };
