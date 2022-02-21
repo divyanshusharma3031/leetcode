3d86230e66e54a6fc2dde5753c77290fb9e6db9e
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-02-21 19:52:12 +0530

    Time: 167 ms (9.11%), Space: 95.5 MB (5.11%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int editdistance(int n,int m,string s1,string s2,vector<vector<int>> &dp)
+    vector<vector<int>> dp;
+    int editdistance(int n,int m,string s1,string s2)
     {
         // cout<<n<<" "<<m<<"\n";
         if((n-1)<0 && (m-1)<0)
@@ -11,12 +12,12 @@ public:
         if((n-1)<0)
         {
             // cout<<m<<"\n";
-            return 1+editdistance(n,m-1,s1,s2,dp);
+            return 1+editdistance(n,m-1,s1,s2);
         }
         if((m-1)<0)
         {
             // cout<<n<<"\n";
-            return 1+editdistance(n-1,m,s1,s2,dp);
+            return 1+editdistance(n-1,m,s1,s2);
         }
         if(dp[n][m])
         {
@@ -24,13 +25,14 @@ public:
         }
         if(s1[n-1]==s2[m-1])
         {
-           return dp[n][m]=editdistance(n-1,m-1,s1,s2,dp);
+           return dp[n][m]=editdistance(n-1,m-1,s1,s2);
         }
         // cout<<dp[n][m]<<"\n";
-        return dp[n][m]=1+min({editdistance(n-1,m,s1,s2,dp),editdistance(n,m-1,s1,s2,dp),editdistance(n-1,m-1,s1,s2,dp)});
+        return dp[n][m]=1+min({editdistance(n-1,m,s1,s2),editdistance(n,m-1,s1,s2),editdistance(n-1,m-1,s1,s2)});
     }
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,0));
-       return editdistance(word1.length(),word2.length(),word1,word2,dp);
+        vector<vector<int>> aux(word1.size()+1,vector<int>(word2.size()+1,0));
+        dp=aux;
+       return editdistance(word1.length(),word2.length(),word1,word2);
     }
 };
