0b5d70a9d93194dddeaaeb2b60ce9f0fdcf3a983
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-03-03 21:36:50 +0530

    Time: 172 ms (5.00%), Space: 289.4 MB (5.00%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
     bool checksub(int n,int m,string s,string t)
     {
         if(n==0)
@@ -10,13 +11,19 @@ public:
         {
             return false;
         }
+        if(dp[n][m]!=-1)
+        {
+            return dp[n][m];
+        }
         if(s[n-1]==t[m-1])
         {
-            return checksub(n-1,m-1,s,t);
+            return dp[n][m]=checksub(n-1,m-1,s,t);
         }
-        return checksub(n,m-1,s,t);
+        return dp[n][m]=checksub(n,m-1,s,t);
     }
     bool isSubsequence(string s, string t) {
+        vector<vector<int>> arr(s.length()+1,vector<int>(t.length()+1,-1));
+        dp=arr;
        return checksub(s.length(),t.length(),s,t); 
     }
 };
