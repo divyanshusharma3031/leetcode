fb22856066b6d2eaaf69a34d1f45e207e63641e3
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-03-09 13:18:30 +0530

    Time: 74 ms (77.14%), Space: 48.4 MB (30.30%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,50 +1,21 @@
 class Solution {
 public:
-    bool jump(int idx,vector<int> &nums,vector<int> &dp)
-    {
-        int n=nums.size();
-        if(idx>=(n-1))
-        {
-            return dp[idx]=true;
-        }
-        if(nums[idx]==0)
-        {
-            return dp[idx]=false;
-        }
-        if(dp[idx]!=-1)
-        {
-            return dp[idx];
-        }
-        int i=1;
-        while(i<=nums[idx])
-        {
-            if(jump(idx+i,nums,dp))
-            {
-                return dp[idx]=true;
-            }
-            i++;
-        }
-        return dp[idx]=false;
-    }
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        if(nums.size()==1)
+        int size=nums.size();
+        int mnjump=0;
+        for(int i=nums.size()-2;i>=0;i--)
         {
-            return true;
-        }
-        int z=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==0)
+            mnjump++;
+            if(nums[i]>=mnjump)
             {
-                z++;
+                mnjump=0;
             }
         }
-        if(z==0)
+        if(mnjump==0)
         {
             return true;
         }
-        vector<int> dp(n+1,-1);
-        return jump(0,nums,dp);
+        
+        return false;
     }
 };
