acff7319e0875359a9fdd6060f9599f11ba15b41
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-03-16 19:18:02 +0530

    Time: 11 ms (86.51%), Space: 10.4 MB (86.39%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int lis(vector<int> &nums,int n,int ele)
-    {
-        // cout<<n<<" "<<ele<<"\n";
-        if(n<=0)
+    int lengthOfLIS(vector<int>& nums) {
+        //NOW i nlog(n)
+        vector<int> res;
+        for(int i=0;i<nums.size();i++)
         {
-            if(n==0 && ele==0 &&  nums[n]<nums[ele])
+            auto it=lower_bound(res.begin(),res.end(),nums[i]);
+            if(it==res.end())
             {
-                return dp[n][ele]=1;
+                res.push_back(nums[i]);
             }
             else
             {
-                return 0;
+                *it=nums[i];
             }
         }
-        if(dp[n][ele]!=-1)
-        {
-            return dp[n][ele];
-        }
-        if(ele==nums.size() || nums[n-1]<nums[ele])
-        {
-            return dp[n][ele]=max(1+lis(nums,n-1,n-1),lis(nums,n-1,ele));
-        }
-        else
-        {
-            return dp[n][ele]=lis(nums,n-1,ele);
-        }
-    }
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        dp.resize(n+1,vector<int>(n+1,-1));
-        return lis(nums,nums.size(),n);
+        return res.size();
     }
 };
