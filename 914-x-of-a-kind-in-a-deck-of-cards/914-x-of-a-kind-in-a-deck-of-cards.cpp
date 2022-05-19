4349a0df2a3f33941bf0064f961831d4dfc16065
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-05-19 16:29:56 +0530

    Time: 23 ms (42.17%), Space: 17.3 MB (88.57%) - LeetHub

---
+++ 914-x-of-a-kind-in-a-deck-of-cards/914-x-of-a-kind-in-a-deck-of-cards.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool hasGroupsSizeX(vector<int>& arr) {
+        map<int,int> mpp;
+        int n=arr.size();
+        for(int i=0;i<n;i++)
+        {
+            mpp[arr[i]]++;
+        }
+        sort(arr.begin(),arr.end());
+        int x=mpp[arr[0]];
+        for(auto it:mpp)
+        {
+            x=__gcd(x,it.second);
+        }
+        if(x==1)
+        {
+            return false;
+        }
+        return true;
+    }
+};
