457ba2c30b1d400d833f343d14f9872d78fb49f2
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-05-21 16:21:46 +0530

    Time: 71 ms (46.44%), Space: 36.2 MB (5.15%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,50 +1,44 @@
+#include<string.h>
 class Solution {
 public:
-    bool ispalindrome(string s)
+    bool palindrome(string s)
     {
         int i=0;
-        int j=s.length()-1;
-        while(i<j)
+        int n=s.length()-1;
+        while(i<=n)
         {
-            if(s[i]!=s[j])
+            if(s[i]!=s[n])
             {
                 return false;
             }
             i++;
-            j--;
+            n--;
         }
         return true;
     }
     bool validPalindrome(string s) {
+        if(palindrome(s))
+        {
+            return true;
+        }
+        int n=s.length();
         int i=0;
-        int j=s.length()-1;
-        int c=0;
-        while(i<j)
+        int j=n-1;
+        while(i<=j)
         {
             if(s[i]!=s[j])
             {
-                c++;
-                if(c>1)
-                {
-                    return false;
-                }
-                
-                if(ispalindrome(s.substr(i+1,j-i)))
-                {
-                    i++;
-                }
-                else if(ispalindrome(s.substr(i,j-i)))
-                {
-                    j--;
-                }
-                else
-                {
-                    return false;
-                }
+                break;
             }
             i++;
             j--;
         }
-        return true;
+        string s1=s.substr(0,i)+s.substr(i+1);
+        string s2=s.substr(0,j)+s.substr(j+1);
+        if(palindrome(s1)|| palindrome(s2))
+        {
+            return true;
+        }
+        return false;
     }
 };
