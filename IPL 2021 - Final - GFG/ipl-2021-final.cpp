7d5e88d45674399894f58030ecb1610a85a7f899
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-05-25 11:05:26 +0530

    Added solution - LeetHub

--- IPL 2021 - Final - GFG/ipl-2021-final.cpp
+++ IPL 2021 - Final - GFG/ipl-2021-final.cpp
@@ -8,48 +8,34 @@ using namespace std;
 // User function template for c++
 class Solution {
   public:
-    int findMaxLen(string str) {
-        int n = str.length();
- 
-    // Create a stack and push -1 as
-    // initial index to it.
-    stack<int> stk;
-    stk.push(-1);
- 
-    // Initialize result
-    int result = 0;
- 
-    // Traverse all characters of given string
-    for (int i = 0; i < n; i++)
-    {
-        // If opening bracket, push index of it
-        if (str[i] == '(')
-            stk.push(i);
-         
-        // If closing bracket, i.e.,str[i] = ')'
-        else
+    int findMaxLen(string s) {
+        int n=s.length();
+        stack<int> st;
+        st.push(-1);
+        int req=0;
+        for(int i=0;i<n;i++)
         {
-            // Pop the previous opening
-            // bracket's index
-            if (!stk.empty())
+            if(s[i]=='(')
             {
-                stk.pop();
+                st.push(i);
             }
-             
-            // Check if this length formed with base of
-            // current valid substring is more than max
-            // so far
-            if (!stk.empty())
-                result = max(result, i - stk.top());
- 
-            // If stack is empty. push current index as
-            // base for next valid substring (if any)
             else
-                stk.push(i);
+            {
+                if(!st.empty())
+                {
+                    st.pop();
+                }
+                if(!st.empty())
+                {
+                   req=max(req,i-st.top()); 
+                }
+                else
+                {
+                    st.push(i);
+                }
+            }
         }
-    }
- 
-    return result;
+        return req;
         // code here
     }
 };
