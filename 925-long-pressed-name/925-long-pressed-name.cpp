2a148063a73033b9f9366fec3c1b7e7ad73ccfc6
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-05-31 16:22:02 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (83.53%) - LeetHub

--- 925-long-pressed-name/925-long-pressed-name.cpp
+++ 925-long-pressed-name/925-long-pressed-name.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
     bool isLongPressedName(string name, string typed) {
-        string s="";
-        int n=name.length();
-        int m=typed.length();
         int i=0;
         int j=0;
+        int n=name.length();
+        int m=typed.length();
+        
         while(i<n)
         {
-            char ele=name[i];
-            int c=1;
-            i++;
-            while(i<n && name[i]==name[i-1])
-            {
-                c++;
-                i++;
-            }
-            int x=0;
-            while(j<m && typed[j]==ele)
-            {
-                j++;
-                x++;
-            }
-            // cout<<c<<" "<<x<<'\n';
-            if(c>x)
+            if(name[i]!=typed[j])
             {
                 return false;
             }
+            int mn=1;
+            while(i<n-1 && name[i]==name[i+1])
+            {
+                mn++;
+                i++;
+            }
+            int w=1;
+            
+            while(j<m-1 && typed[j]==typed[j+1])
+            {
+                w++;
+                j++;
+            }
+            if(w<mn)
+            {
+                return false;
+            }
+            i++;
+            j++;
         }
         if(j!=m)
         {
             return false;
         }
-         return true;
+        return true;
     }
 };
