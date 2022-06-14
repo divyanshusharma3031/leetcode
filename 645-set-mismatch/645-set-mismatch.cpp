d11f5aefa92387996bb92a44d0609f62051210e2
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-06-14 18:04:36 +0530

    Time: 85 ms (27.68%), Space: 30 MB (33.75%) - LeetHub

---
+++ 645-set-mismatch/645-set-mismatch.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int x=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            x=x^(nums[i])^(i+1);
+        }
+        map<int,int> mpp;
+        int b=0;
+        for(int i=0;i<n;i++)
+        {
+            mpp[nums[i]]++;
+            if(mpp[nums[i]]==2)
+            {
+                b=nums[i];
+                break;
+            }
+        }
+        return {b,x^b};
+    }
+};
