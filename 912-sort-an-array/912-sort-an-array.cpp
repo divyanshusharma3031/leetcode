a86c40b90664b70155f5a954d42e742a0e533289
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-06-14 16:45:10 +0530

    Time: 75 ms (72.39%), Space: 29 MB (40.34%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
-        return arr;
+    vector<int> sortArray(vector<int>& nums) {
+        stable_sort(nums.begin(), nums.end());
+        return nums;
     }
 };
