f7b0e8024ffeeca594244065ecf289a6119ab7a9
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-06-17 17:00:13 +0530

    Time: 463 ms (7.27%), Space: 80.4 MB (13.38%) - LeetHub

---
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& arr, int k) {
+         map<int,int> mpp;
+         if(arr.size()==1)
+         {
+             return false;
+         }
+         if(k>=arr.size())
+         {
+             k=arr.size()-1;
+         }
+         for(int i=0;i<=k;i++)
+         {
+             mpp[arr[i]]++;
+             if(mpp[arr[i]]>=2)
+             {
+                 return true;
+             }
+         }
+        
+         int n=arr.size();
+         int j=0;
+         for(int i=k+1;i<n;i++)
+         {
+             mpp[arr[j]]--;
+             mpp[arr[i]]++;
+             if(mpp[arr[i]]>=2)
+             {
+                 return true;
+             }
+             j++;
+         }
+        return false;
+    }
+};
