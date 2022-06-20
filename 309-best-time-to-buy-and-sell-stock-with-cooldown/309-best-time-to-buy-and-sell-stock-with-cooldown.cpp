0eec62e001111d04161790f4597d5ad1b50ff69e
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-06-20 14:40:08 +0530

    Time: 14 ms (19.91%), Space: 11.1 MB (97.95%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    vector<int> dp;
-    int mxpro(vector<int> &prices,int n,int i)
-    {
-        if(i>=n)
-        {
-            return 0;
-        }
-        if(dp[i]!=-1)
-        {
-            return dp[i];
-        }
-        int ans=0;
-        for(int j=i;j<n;j++)
-        {
-            ans=max(ans,prices[j]-prices[i]+mxpro(prices,n,j+2));
-        }
-        ans=max(ans,mxpro(prices,n,i+1));
-        return dp[i]=ans;
-    }
     int maxProfit(vector<int>& prices) {
+        int bsp=-prices[0];
+        int ssp=0;
+        int csp=0;
         int n=prices.size();
-        dp.resize(n+1,-1);
-        return mxpro(prices,n,0); 
+        for(int i=1;i<n;i++)
+        {
+            bsp=max(bsp,csp-prices[i]);
+            csp=max(ssp,csp);
+            ssp=max(ssp,bsp+prices[i]);
+        }
+        return ssp;
     }
 };
