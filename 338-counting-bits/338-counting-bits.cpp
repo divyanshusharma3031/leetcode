afc77a74cc135e42af1d3137fb48c2028d088843
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-06-21 16:02:04 +0530

    Time: 4 ms (86.83%), Space: 7.8 MB (87.88%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -5,14 +5,7 @@ public:
         ans[0]=0;
         for(int i=1;i<=n;i++)
         {
-            if(i%2)
-            {
-                ans[i]=ans[i-1]+1;
-            }
-            else
-            {
-                ans[i]=__builtin_popcount(i);
-            }
+            ans[i]=ans[i/2]+i%2;
         }
         return ans;
     }
