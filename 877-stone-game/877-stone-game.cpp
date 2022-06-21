bee4e7e83e7168dbb11021a10d627b929a645f26
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-06-21 19:12:50 +0530

    Time: 3 ms (80.44%), Space: 7.6 MB (52.50%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,34 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int calc(vector<int>&piles,int lo,int hi,int s1,bool b)
-    {
-        if(lo>hi)
-        {
-            return 0;
-        }
-        if(dp[lo][hi]!=-1)
-        {
-            return dp[lo][hi];
-        }
-        if(b)
-        {
-            return dp[lo][hi]=max(piles[lo]+calc(piles,lo+1,hi,s1,!b),piles[hi]+calc(piles,lo,hi-1,s1,!b));
-        }
-        return dp[lo][hi]=max(calc(piles,lo+1,hi,s1,!b),calc(piles,lo,hi-1,s1,!b));
-    }
     bool stoneGame(vector<int>& piles) {
-        int n=piles.size();
-        dp.resize(n+1,vector<int>(n+1,-1));
-        int lo=0;
-        int hi=n-1;
-        int s=0;
-        for(int i=0;i<n;i++)
-        {
-            s+=piles[i];
-        }
-        int a1=calc(piles,lo,hi,0,true);
-        int a2=s-a1;
-        return a1>a2;
+        return true;
     }
 };
