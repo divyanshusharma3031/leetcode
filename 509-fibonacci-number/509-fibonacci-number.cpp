0726aa7b9ed198fc8ae400f2ac351b6a0c2a565c
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-06-22 15:46:56 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (17.94%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,20 +1,7 @@
 class Solution {
 public:
-    int fib(int n) {
-        if(n==0 || n==1)
-        {
-            return n;
-        }
-        int p=1;
-        int pp=0;
-        int ans=0;
-        for(int i=2;i<=n;i++)
-        {
-            ans=p+pp;
-            int temp=p;
-            p=ans;
-            pp=temp;
-        }
-        return ans;
+    int fib(int N) {
+         double phi = (sqrt(5) + 1) / 2;     
+        return round(pow(phi, N) / sqrt(5));
     }
 };
