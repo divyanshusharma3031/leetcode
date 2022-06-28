8e8bd6c5f515646ada113833051991a5c68ef3bf
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-06-28 15:22:55 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -31,29 +31,32 @@ class Solution
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-       vector<vector<int>> dp(n+1,vector<int>(W+1,0));
+       vector<int> dp(W+1,0);
           for(int i=0;i<=W;i++)
           {
               if(i>=wt[0])
               {
-                  dp[0][i]=val[0];
+                  dp[i]=val[0];
               }
           }
        for(int i=1;i<n;i++)
        {
+           vector<int> temp(W+1,0);
            for(int j=0;j<=W;j++)
            {
                if((j-wt[i])>=0)
                {
-                   dp[i][j]=max(val[i]+dp[i-1][j-wt[i]],dp[i-1][j]);
+                   temp[j]=max(val[i]+dp[j-wt[i]],dp[j]);
                }
                else
                {
-                   dp[i][j]=dp[i-1][j];
+                   temp[j]=dp[j];
                }
            }
+           dp=temp;
        }
-       return dp[n-1][W];
+    //   space optimised
+       return dp[W];
     }
 };
 
