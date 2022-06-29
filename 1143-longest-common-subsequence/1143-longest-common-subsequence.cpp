894a03b2a39c57df4c94cf681ae2ce0518871aea
divyanshusharma3031 <89530118+divyanshusharma3031@users.noreply.github.com>
2022-06-29 13:38:08 +0530

    Time: 43 ms (33.14%), Space: 13.3 MB (10.29%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int lcs(int n,int m,string &text1,string &text2,vector<vector<int>> &dp)
+    int lcs(string &text1,string &text2,int n,int m,vector<vector<int>> &dp)
     {
         if(n==0 || m==0)
         {
             return 0;
         }
-        // cout<<text1[n-1]<<" "<<text2[m-1]<<"\n";
+        if(text1[n-1]==text2[m-1])
+        {
+            return dp[n][m]=1+lcs(text1,text2,n-1,m-1,dp);
+        }
         if(dp[n][m]!=-1)
         {
             return dp[n][m];
         }
-        if(text1[n-1]==text2[m-1])
-        {
-            return dp[n][m]=1+lcs(n-1,m-1,text1,text2,dp);
-        }
-        return dp[n][m]=max(lcs(n-1,m,text1,text2,dp),lcs(n,m-1,text1,text2,dp));
+        return dp[n][m]=max(lcs(text1,text2,n-1,m,dp),lcs(text1,text2,n,m-1,dp));
     }
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return lcs(n,m,text1,text2,dp);
+        return lcs(text1,text2,n,m,dp);
     }
 };
