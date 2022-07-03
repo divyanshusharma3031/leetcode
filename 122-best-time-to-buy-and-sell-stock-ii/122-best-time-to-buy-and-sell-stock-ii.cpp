847034ef65dd77a8bb3ce53b1acabd2b26d3bf8a
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-07-03 23:05:36 +0530

    Time: 20 ms (13.73%), Space: 15.3 MB (13.91%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -20,6 +20,23 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         dp.resize(prices.size()+1,vector<int>(2,-1));
-        return pro(prices,0,1);
+        int n=prices.size();
+        dp[n][0]=0;
+        dp[n][1]=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<2;j++)
+            {
+                if(j)
+                {
+                    dp[i][j]=max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+                }
+                else
+                {
+                    dp[i][j]=max(prices[i]+dp[i+1][1],dp[i+1][0]);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
