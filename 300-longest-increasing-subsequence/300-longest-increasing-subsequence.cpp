fd54abd0f9c52394abdd54853290f5c2ed2b513a
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-07-05 12:58:05 +0530

    Time: 309 ms (59.87%), Space: 10.6 MB (23.44%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,56 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int lis(vector<int> &nums,int i,int j)
-    {
-        int n=nums.size();
-        if(i==n)
-        {
-            return 0;
-        }
-        if(j==-1)
-        {
-            if(dp[i][n+1]!=-1)
-            {
-                return dp[i][n+1];
-            }
-            return dp[i][n+1]=max(1+lis(nums,i+1,i),lis(nums,i+1,j));
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        if(nums[i]>nums[j])
-        {
-            return dp[i][j]=max(1+lis(nums,i+1,i),lis(nums,i+1,j));
-        }
-        return dp[i][j]=(lis(nums,i+1,j));
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        dp.resize(n+1,vector<int>(n+2,0));
-        for(int j=0;j<n+2;j++)
+        
+        vector<int> len(n+1,1);
+        
+        for(int i=0;i<n;i++)
         {
-            dp[n][j]=0;
-        }
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=i-1;j>=-1;j--)
+            int m=0;
+            for(int j=0;j<i;j++)
             {
-                if(j==-1)
+                if(nums[j]<nums[i])
                 {
-                    dp[i][j+1]=max(1+dp[i+1][i+1],dp[i+1][j+1]);
-                }
-                else if(nums[i]>nums[j])
-                {
-                    dp[i][j+1]=max(1+dp[i+1][i+1],dp[i+1][j+1]);
-                }
-                else
-                {
-                    dp[i][j+1]=dp[i+1][j+1];
+                    m=max(m,len[j]);
                 }
             }
+            len[i]=len[i]+m;
         }
-        return dp[0][0];
+        int mx=0;
+        for(int i=0;i<=n;i++)
+        {
+            mx=max(mx,len[i]);
+        }
+        return mx;
     }
 };
