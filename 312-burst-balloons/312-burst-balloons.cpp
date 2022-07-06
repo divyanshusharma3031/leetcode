3272fb122c5557d60029f1a91c3d92fee3d4dc5e
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-07-06 15:11:06 +0530

    Time: 1006 ms (12.71%), Space: 10.4 MB (52.50%) - LeetHub

---
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int pro(vector<int> &nums,int i,int j)
+    {
+        if(i>j)
+        {
+            return 0;
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        int maxi=-12;
+        for(int ind=i;ind<=j;ind++)
+        {
+            int cost=nums[i-1]*nums[ind]*nums[j+1]+pro(nums,i,ind-1)+pro(nums,ind+1,j);
+            maxi=max(maxi,cost);
+        }
+        return dp[i][j]=maxi;
+    }
+    
+    int maxCoins(vector<int>& nums) {
+        int n=nums.size();
+        nums.push_back(1);
+        reverse(nums.begin(),nums.end());
+        nums.push_back(1);
+        reverse(nums.begin(),nums.end());
+        dp.resize(n+1,vector<int>(n+1,-1));
+        return pro(nums,1,n);
+    }
+};
