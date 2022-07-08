b1eef87bb7081708bbaa58c0c3b027e2e1658f74
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-07-08 18:12:02 +0530

    Time: 11 ms (7.22%), Space: 8.9 MB (12.12%) - LeetHub

---
+++ 66-plus-one/66-plus-one.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        reverse(digits.begin(),digits.end());
+        int n=digits.size();
+        digits[0]=digits[0]+1;
+        int c=digits[0]/10;
+        digits[0]=digits[0]%10;
+        int i=1;
+        while(i<n)
+        {
+            digits[i]=digits[i]+c;
+            c=digits[i]/10;
+            digits[i]=digits[i]%10;
+            i++;
+        }
+        if(c!=0)
+        {
+            digits.push_back(c);
+        }
+        reverse(digits.begin(),digits.end());
+        return digits;
+    }
+};
