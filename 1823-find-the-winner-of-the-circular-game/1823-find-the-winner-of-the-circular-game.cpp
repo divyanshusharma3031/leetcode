e99e7992f4d26ed40740d87be9bcbe8df9c10f85
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-07-09 17:12:07 +0530

    Time: 3 ms (76.57%), Space: 5.8 MB (90.37%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,40 +1,15 @@
 class Solution {
 public:
-    int findTheWinner(int n, int k) {
+    int wimnner(int n,int k)
+    {
         if(n==1)
         {
-            return 1;
+            return 0;
         }
-        set<int> s;
-        for(int i=1;i<=n;i++)
-        {
-            s.insert(i);
-        }
-        int i=0;
-        int c=0;
-        while(c<k)
-        {
-            i++;
-            c++;
-        }
-        // cout<<i<<"\n";
-        s.erase(i);
-        while(s.size()>1)
-        {
-            c=0;
-            while(c<k)
-            {
-                i=(i+1)%(n+1);
-                while(s.find(i)==s.end())
-                {
-                    i=(i+1)%(n+1);
-                }
-                
-                c++;
-            }
-            // cout<<i<<"\n";
-            s.erase(i);
-        }
-        return *s.begin();
+        return (wimnner(n-1,k)+k)%n;
+    }
+    int findTheWinner(int n, int k) {
+//         recursive solution
+        return wimnner(n,k)+1;
     }
 };
