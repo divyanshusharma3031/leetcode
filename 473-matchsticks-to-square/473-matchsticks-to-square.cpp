0e82eddb005c2cc5f269fa2d53bd6f2f5fe4b4a1
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-07-12 11:22:32 +0530

    Time: 1229 ms (9.02%), Space: 10.2 MB (47.49%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int target=0;
+    bool possible(vector<int> &v,vector<int> &arr,int idx)
+    {
+        int n=arr.size();
+        if(idx==n)
+        {
+            return (v[0]==v[1]) && (v[1]==v[2]) && (v[2]==v[3]);
+        }
+        
+        for(int i=0;i<4;i++)
+        {
+            if((v[i]+arr[idx])>target)
+            {
+                continue;
+            }
+            v[i]+=arr[idx];
+            if(possible(v,arr,idx+1))
+            {
+                return true;
+            }
+            v[i]-=arr[idx];
+        }
+        return false;
+    }
+    bool makesquare(vector<int>& arr) {
+        int s=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++)
+        {
+            s+=arr[i];
+        }
+        if((s%4)!=0)
+        {
+            return false;
+        }
+        sort(arr.begin(),arr.end(),greater<int>());
+        target=s/4;
+        vector<int> v(4,0);
+        return possible(v,arr,0);
+    }
+};
