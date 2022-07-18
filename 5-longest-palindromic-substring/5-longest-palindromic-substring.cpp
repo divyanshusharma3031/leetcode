7b7bdaf6159834fdefb5f95a465e414bf891d7d8
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-07-18 11:07:52 +0530

    Time: 517 ms (27.48%), Space: 385.1 MB (11.43%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n=s.size();
-        vector<vector<bool>> dp(n+1,vector<bool>(n+1,false));
-        for(int i=0;i<n;i++)
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int i=0;i<=n;i++)
         {
-            dp[i][i]=true;
+            dp[i][i]=1;
         }
-        string ans;
+        string ans="";
         ans+=s[0];
         for(int i=n-1;i>=0;i--)
         {
@@ -17,9 +17,12 @@ public:
                 {
                     if((j-i)==1 || dp[i+1][j-1])
                     {
-                        dp[i][j]=true;
-                        if(ans.size()<(j-i+1))
+                        // cout<<j<<" "<<i<<"\n";
+                        dp[i][j]=1;
+                        if((j-i+1)>ans.size())
+                        {
                             ans=s.substr(i,j-i+1);
+                        }
                     }
                 }
             }
