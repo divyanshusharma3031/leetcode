ab7fd9036b121e83be064a9f075549141f33cc9f
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-07-28 12:27:39 +0530

    Time: 8 ms (82.77%), Space: 7.1 MB (99.64%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,9 +1,25 @@
+typedef vector<int> vi;
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-//         nlogn approach
-        return t==s;
+        vi arr1(26,0);
+        vi arr2(26,0);
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            arr1[s[i]-'a']++;
+            arr2[t[i]-'a']++;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(arr1[i]!=arr2[i])
+            {
+                return false;
+            }
+        }
+        return true;
     }
 };
