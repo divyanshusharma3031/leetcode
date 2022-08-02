9e131e23a5bc3057f54abe8a83e381355cde38f7
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-08-02 22:54:35 +0530

    Time: 18 ms (27.38%), Space: 9.5 MB (56.99%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,21 +1,49 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        // first with extra space
-        // sc:o(n) t.c:o(n)
-        unordered_map<int,int> mpp;
-        for(int i=0;i<nums.size();i++)
+    int singleNumber(vector<int>& arr) {
+        vector<int> bits(33,0);
+        for(int i=0;i<32;i++)
         {
-            mpp[nums[i]]++;
-        }
-        vector<int> ans;
-        for(auto it:mpp)
-        {
-            if(it.second==1)
+            for(auto x:arr)
             {
-                return it.first;
+                long long it=abs(x);
+                if(x&(1<<i))
+                {
+                    bits[i]++;
+                }
             }
         }
-        return -1;
+        long long ans=0;
+        for(int i=0;i<32;i++)
+        {
+            if(bits[i]%3==0)
+            {
+                bits[i]=0;
+            }
+            else
+            {
+                bits[i]=1;
+            }
+        }
+        for(int i=0;i<32;i++)
+        {
+            ans=ans+pow(2,i)*bits[i];
+        }
+        int  c=0;
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            if(ans==arr[i])
+            {
+                c++;
+            }
+        }
+        if(c==1)
+        {
+            return ans;
+        }
+        ans=-ans;
+        // cout<<ans<<"\n";
+        return -ans;
     }
 };
