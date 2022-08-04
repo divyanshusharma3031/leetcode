3116610807543bad94b745b7d6750eabd36c25d3
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-08-04 15:13:07 +0530

    Time: 43 ms (61.79%), Space: 28.2 MB (60.48%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,33 +1,47 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int i=0;
-        int j=0;
-        int ans=1e9;
-        int s=0;
+        // O(nlogn) binary search approach;
+        int ans=INT_MAX;
         int n=nums.size();
-        while(j<n)
+        for(int i=1;i<n;i++)
         {
-            s+=nums[j];
-            if(s>=target)
-            {
-                while(i<n && (s-nums[i])>=target)
-                {
-                    s=s-nums[i];
-                    i++;
-                }
-                ans=min(ans,j-i+1);
-            }
-            j++;
+            nums[i]=nums[i-1]+nums[i];
         }
-        if(s>=target)
+        for(int i=n-1;i>=0 && nums[i]>=target;i--)
         {
-            ans=min(ans,j-i+1);
+            int j=upper_bound(nums.begin(),nums.end(),nums[i]-target)-nums.begin();
+            ans=min(ans,i-j+1);
         }
-        if(ans==1e9)
-        {
-            return 0;
-        }
-        return ans;
+        return ans==INT_MAX?0:ans;
     }
 };
+// O(n) variable length sliding window
+        // int i=0;
+        // int j=0;
+        // int ans=1e9;
+        // int s=0;
+        // int n=nums.size();
+        // while(j<n)
+        // {
+        //     s+=nums[j];
+        //     if(s>=target)
+        //     {
+        //         while(i<n && (s-nums[i])>=target)
+        //         {
+        //             s=s-nums[i];
+        //             i++;
+        //         }
+        //         ans=min(ans,j-i+1);
+        //     }
+        //     j++;
+        // }
+        // if(s>=target)
+        // {
+        //     ans=min(ans,j-i+1);
+        // }
+        // if(ans==1e9)
+        // {
+        //     return 0;
+        // }
+        // return ans;
