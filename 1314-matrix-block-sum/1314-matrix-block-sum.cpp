5f5f580aad45db193d15fd17830ccbd0e816c0ca
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-08-09 17:51:59 +0530

    Time: 7 ms (97.16%), Space: 9.4 MB (74.36%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -1,26 +1,48 @@
 class Solution {
 public:
+    int n;
+    int m;
+    int element(int i,int j,vector<vector<int>>& mat)
+    {
+        if(i<0 || j<0)
+        {
+            return 0;
+        }
+        if(i>=n)
+        {
+            i=n-1;
+        }
+        if(j>=m)
+        {
+            j=m-1;
+        }
+        return mat[i][j];
+    }
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
-        int n=mat.size();
-        int m=mat[0].size();
+//        prefix sum in 2d matrix;
+        
+        n=mat.size();
+        
+        m=mat[0].size();
+        
+        // vector<vector<int>> mat(n,vector<int>(m,0));
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                mat[i][j]=mat[i][j]+element(i-1,j,mat)+element(i,j-1,mat)-element(i-1,j-1,mat);
+            }
+        }
         vector<vector<int>> ans(n,vector<int>(m,0));
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                int r1=max(i-k,0);
-                int r2=min(i+k,n-1);
-                int c1=max(j-k,0);
-                int c2=min(j+k,m-1);
-                for(int r=r1;r<=r2;r++)
-                {
-                    for(int c=c1;c<=c2;c++)
-                    {
-                        ans[i][j]+=mat[r][c];
-                    }
-                }
+                ans[i][j]=element(i+k,j+k,mat)-element(i+k,j-k-1,mat)-element(i-k-1,j+k,mat)+element(i-k-1,j-k-1,mat);
             }
         }
+        
         return ans;
     }
 };
