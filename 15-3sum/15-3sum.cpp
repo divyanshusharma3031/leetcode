7c07f878b112e4c078f23673bb425cc59197e6df
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-08-11 20:14:20 +0530

    Time: 73 ms (97.24%), Space: 19.9 MB (92.15%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,35 +1,37 @@
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
         vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        for(int i=0;i<n-2;i++)
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for (int i = 0; i < n - 2; i++)
         {
-            int t=-nums[i];
-            int j=i+1;
-            int k=n-1;
-            if(i==0 || (i>0 && nums[i]!=nums[i-1]))
+            int t = -nums[i];
+            int j = i + 1;
+            int k = n - 1;
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1]))
             {
-            while(j<k )
-            {
-                
-                    int s=nums[j]+nums[k];
-                    if(s==t)
+                while (j < k)
+                {
+
+                    int s = nums[j] + nums[k];
+                    if (s == t)
                     {
-                        ans.push_back({nums[i],nums[j],nums[k]});
-                        while(j<k && nums[j]==nums[j+1])
+                        ans.push_back({nums[i], nums[j], nums[k]});
+                        while (j < k && nums[j] == nums[j + 1])
                         {
                             j++;
                         }
-                        while(k>j && nums[k]==nums[k-1])
+                        while (k > j && nums[k] == nums[k - 1])
                         {
                             k--;
                         }
                         j++;
                         k--;
                     }
-                    else if(s<t)
+                    else if (s < t)
                     {
                         j++;
                     }
@@ -37,9 +39,7 @@ public:
                     {
                         k--;
                     }
-            }
-                
-               
+                }
             }
         }
         return ans;
