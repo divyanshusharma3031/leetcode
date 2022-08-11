ba7a6e32180eaa9c4ff1065dbca08aafddf4a937
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-08-11 18:14:02 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (26.18%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,37 +1,72 @@
 class Solution {
 public:
-    int strStr(string &source, string &target) {
-        // Write your code here
-        int n=source.length();
-        if(n==0)
+    void lpsgenerate(string b, vector<int> &lps)
+    {
+        lps[0] = 0;
+        int i = 1;
+        int len = 0;
+        int n = b.length();
+        while (i < n)
         {
-            if(source==target)
+            if (b[i] == b[len])
             {
-                return 0;
+                len++;
+                lps[i] = len;
+                i++;
             }
+            else
+            {
+                if (len != 0)
+                {
+                    len = lps[len-1];
+                }
+                else
+                {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+    int strStr(string &a, string &b) {
+        // Write your code here
+        int m = b.length();
+        vector<int> lps(m, 0);
+        int N=a.length();
+        lpsgenerate(b, lps);
+        int i = 0;
+        int j = 0;
+        bool x=false;
+        vector<int> ans;
+        while (i < a.length())
+        {
+            if (b[j] == a[i])
+            {
+                j++;
+                i++;
+            }
+            if(j==m)
+            {
+                x=true;
+                ans.push_back(i-j+1);
+                j=lps[j-1];
+            }
+            else if(i<N && b[j]!=a[i])
+            {
+                if(j!=0)
+                {
+                    j=lps[j-1];
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+        if(!x)
+        {
             return -1;
         }
-        int m=target.length();
-        if(m==0)
-        {
-            return 0;
-        }
-        for(int i=0;i<n;i++)
-        {
-            string temp="";
-            for(int j=i;j<n;j++)
-            {
-                temp+=source[j];
-                if(temp.size()==target.size() && temp==target)
-                {
-                    return i;
-                }
-                if(temp.size()>target.size())
-                {
-                    break;
-                }
-            }
-        }
-        return -1;
+        return ans[0]-1;
     }
 };
