5de09cf975cd7f4aa2c06ac1209b257675a83f31
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-08-13 14:55:50 +0530

    Time: 99 ms (5.39%), Space: 34 MB (21.73%) - LeetHub

---
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& arr, int n) {
+        map<int,int> planted;
+        map<int,int> space;
+        int N=arr.size();
+        for(int i=0;i<N;i++)
+        {
+            // cout<<arr[i]<<" ";
+            if(arr[i]==1)
+            {
+                planted[i]++;
+            }
+            else
+            {
+                space[i]++;
+            }
+        }
+        int c=0;
+        for(auto it:space)
+        {
+            int x=it.first;
+            // cout<<x<<" ";
+            if(planted[x-1] || planted[x+1])
+            {
+                
+            }
+            else
+            {
+                planted[x]++;
+                c++;
+            }
+        }
+        return c>=n;
+    }
+};
