753836959ce05d09f6132a859c798e6a980a6add
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-08-15 21:17:09 +0530

    Time: 1074 ms (11.87%), Space: 10.4 MB (52.71%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -11,7 +11,7 @@ public:
         {
             return dp[i][j];
         }
-        int maxi=-12;
+        int maxi=-2;
         for(int ind=i;ind<=j;ind++)
         {
             int cost=nums[i-1]*nums[ind]*nums[j+1]+pro(nums,i,ind-1)+pro(nums,ind+1,j);
@@ -19,14 +19,13 @@ public:
         }
         return dp[i][j]=maxi;
     }
-    
     int maxCoins(vector<int>& nums) {
         int n=nums.size();
         nums.push_back(1);
         reverse(nums.begin(),nums.end());
         nums.push_back(1);
         reverse(nums.begin(),nums.end());
-        dp.resize(n+1,vector<int>(n+1,-1));
+        dp.resize(n+2,vector<int>(n+2,-1));
         return pro(nums,1,n);
     }
 };
