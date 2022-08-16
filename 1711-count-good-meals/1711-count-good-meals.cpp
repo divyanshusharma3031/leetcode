c15d758a09a0eb2dbf3f3f77a6eb4a40a7be2b89
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-08-16 08:51:07 +0530

    Time: 349 ms (85.96%), Space: 61.6 MB (21.79%) - LeetHub

---
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    typedef long long ll;
+    const int mod=1e9+7;
+    int countPairs(vector<int>& arr) {
+        long long ans=0;
+        map<ll,ll> mpp;
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+            {
+                mpp[arr[i]]++;
+                continue;
+            }
+            ll x=ceil(log2(arr[i]+1));
+            ll w=ceil(pow(2,x));
+            ll f=w-arr[i];
+            ll y=ceil(log2(arr[i]));
+            ll z=ceil(pow(2,y));
+            ll f2=z-arr[i];
+            if(mpp[f])
+            {
+                // cout<<f<<" "<<arr[i]<<" "<<mpp[f]<<"\n";
+                ans=(ans+mpp[f])%mod;
+            }
+            if(mpp[f2] && f!=f2)
+            {
+                // cout<<f2<<" "<<arr[i]<<" "<<mpp[f2]<<"\n";
+                ans=(ans+mpp[f2])%mod;
+            }
+            mpp[arr[i]]++;
+        }
+        return ans;
+    }
+};
