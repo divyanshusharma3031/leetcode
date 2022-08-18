b022bc2fab3e44259d0bbf82b9f41717b585dc1b
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-08-18 20:27:51 +0530

    Time: 679 ms (5.01%), Space: 82.9 MB (13.94%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        map<int,int> mpp;
+        int n=arr.size();
+        for(int i=0;i<n;i++)
+        {
+            mpp[arr[i]]++;
+        }
+        vector<pair<int,int>> v;
+        for(auto it:mpp)
+        {
+            v.push_back({it.second,it.first});
+        }
+        sort(v.begin(),v.end());
+        reverse(v.begin(),v.end());
+        int x=n;
+        for(int i=0;i<v.size();i++)
+        {
+            n=n-v[i].first;
+            if(n<=x/2)
+            {
+                return i+1;
+            }
+        }
+        return n/2;
+    }
+};
