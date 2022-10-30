79ec32d4b9be683c0e528d3ab5ab077a479dc712
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-10-30 15:22:46 +0530

    Time: 67 ms (84.62%), Space: 29.3 MB (31.62%) - LeetHub

---
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isGoodArray(vector<int>& nums) {
+        int res=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            res=__gcd(res,nums[i]);
+        }
+        return res==1;
+    }
+};
