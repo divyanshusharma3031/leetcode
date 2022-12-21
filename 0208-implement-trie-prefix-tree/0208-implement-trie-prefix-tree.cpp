18f77b9e50af9cdd003ff196222f2da3cc0f65ed
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2022-12-21 23:13:44 +0530

    Time: 149 ms (42.92%), Space: 55 MB (12.70%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,60 @@
+class Trie {
+public:
+    Trie() {}
+    void insert(string word) {
+        Trie* node = this;
+        for (char ch : word) {
+            ch -= 'a';
+            if (!node->next[ch]) { node->next[ch] = new Trie(); }
+            node = node->next[ch];
+        }
+        node->isword = true;
+    }
+    bool search(Trie *node,string word,int i)
+    {
+        if(i==word.size())
+        {
+            return node->isword;
+        }
+        if(word[i]=='.')
+        {
+            bool b=false;
+            for(int j=0;j<26;j++)
+            {
+                if(!node->next[j])
+                {
+                }
+                else
+                {
+                    b=b|search(node->next[j],word,i+1);
+                }
+            }
+            return b;
+        }
+        else
+        {
+            int x=word[i]-'a';
+            if(!node->next[x])
+            {
+                return false;
+            }
+            return search(node->next[x],word,i+1);
+        }
+    }
+    bool search(string word) {
+        Trie* node = this;
+        return search(node,word,0);
+    }
+     bool startsWith(string prefix) {
+        Trie* node = this;
+        for (char ch : prefix) {
+            ch -= 'a';
+            if (!node->next[ch]) { return false; }
+            node = node->next[ch];
+        }
+        return true;
+    }
+private:
+    Trie* next[26] = {};
+    bool isword = false;
+};
