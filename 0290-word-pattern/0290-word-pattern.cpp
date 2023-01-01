128eaed1a39c2e32d6994b285d7f081597747b17
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2023-01-01 20:36:30 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (82.12%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        s+= ' ';
-        int n = pattern.size();
+        s+=' ';
+        int n=pattern.size();
         vector<string> ans;
-
-        int num = s.size();
-        string temp = "";
+        int num=s.size();
+        string temp ="";
         for(int i=0; i<num; i++){
-            if(s[i] == ' ') {
+            if(s[i]==' ') {
                 ans.push_back(temp);
                 temp = "";
             }
-            else temp+= s[i];
+            else
+            {
+                temp+= s[i];
+            }
         }
 
-        if(ans.size() != n) return false;
+        if(ans.size() != n) 
+        {
+            return false;
+        }
         
         for(int i=0; i<n-1; i++){
             for(int j=i+1; j<n; j++){
-                if(pattern[i] == pattern[j] && ans[i] != ans[j] || pattern[i] != pattern[j] && ans[i] == ans[j]) return false;
+                if(pattern[i] == pattern[j] && ans[i] != ans[j] || pattern[i] != pattern[j] && ans[i] == ans[j]){
+                    return false;
+                }
             }
         }
         return true;
