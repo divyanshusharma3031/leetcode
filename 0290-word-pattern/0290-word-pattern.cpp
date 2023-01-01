ac1ffafd6a6809f26c9076fdacf90351d774177b
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2023-01-01 19:27:09 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (19.87%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        s+= ' ';
+        int n = pattern.size();
+        vector<string> ans;
+
+        int num = s.size();
+        string temp = "";
+        for(int i=0; i<num; i++){
+            if(s[i] == ' ') {
+                ans.push_back(temp);
+                temp = "";
+            }
+            else temp+= s[i];
+        }
+
+        if(ans.size() != n) return false;
+        
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(pattern[i] == pattern[j] && ans[i] != ans[j] || pattern[i] != pattern[j] && ans[i] == ans[j]) return false;
+            }
+        }
+        return true;
+    }
+};
