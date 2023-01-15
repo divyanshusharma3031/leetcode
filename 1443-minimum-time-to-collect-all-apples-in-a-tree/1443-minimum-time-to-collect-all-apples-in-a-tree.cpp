4b582b05d64c209241b6fcaade009bea53451342
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2023-01-15 21:00:44 +0530

    Time: 214 ms (31.21%), Space: 60.7 MB (41.80%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,43 +1,52 @@
 class Solution {
 public:
-    int mn=1e9;
-    int dfs(int node,vector<int> adj[],vector<int> &vis,vector<bool> &hasApple)
+    int helper(int n, vector<int> edges[], vector<bool>& hasApple,int cv,bool* visited) 
+{
+    visited[cv]=true;
+    int ans=0;
+    // for(int i=0;i<es;i++)
+    // {
+    //     if(edges[i][0]==cv && !visited[edges[i][1]])
+    //     {
+    //         int temp=helper(n,edges, hasApple,edges[i][1],visited);
+    //         if(temp!=-1){ans+=2+temp;}
+    //     }
+    //     if(edges[i][1]==cv && !visited[edges[i][0]])
+    //     {
+    //         int temp=helper(n,edges, hasApple,edges[i][0],visited);
+    //         if(temp!=-1){ans+=2+temp;}
+    //     }
+    // }
+        for(auto it:edges[cv])
+        {
+            if(!visited[it])
+            {
+                int temp=helper(n,edges,hasApple,it,visited);
+                if(temp!=-1)
+                {
+                    ans=ans+2+temp;
+                }
+            }
+        }
+    if(ans==0)
     {
-        if(vis[node])
-        {
-            return 0;
-        }
-        vis[node]=1;
-        int ans=0;
-        int c=0;
-        if(hasApple[node])
-        {
-            c=c+2;
-        }
-        for(auto it:adj[node])
-        {
-            ans=ans+dfs(it,adj,vis,hasApple);
-        }
-        if(c==0 && ans!=0)
-        {
-            c=2;
-        }
-        // cout<<node<<" "<<c<<"\n";
-        return c+ans;
+        if(hasApple[cv])
+        {return 0;}
+        if(cv==0){return 0;}
+        return -1;
     }
-    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
-        vector<int> adj[n+1];
-        for(int i=0;i<edges.size();i++)
-        {
-            adj[edges[i][0]].push_back(edges[i][1]);
-            adj[edges[i][1]].push_back(edges[i][0]);
-        }
-        vector<int> vis(n,0);
-        int ans=dfs(0,adj,vis,hasApple);
-        if(ans==0)
-        {
-            return 0;
-        }
-        return ans-2;
+    return ans;
+}
+int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) 
+{
+    bool visited[n];
+    memset(visited,false,sizeof(visited));
+    vector<int> adj[n+1];
+    for(int i=0;i<edges.size();i++)
+    {
+        adj[edges[i][0]].push_back(edges[i][1]);
+        adj[edges[i][1]].push_back(edges[i][0]);
     }
+    return helper(n,adj,hasApple,0,visited);
+}
 };
