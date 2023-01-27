34ad71cc0f69dca904b8e122491987e4ff67bf64
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2023-01-27 12:30:29 +0530

    Time: 2849 ms (5.03%), Space: 256.5 MB (9.13%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    map<string,int> mpp;
+    map<string,int> dp;
+    bool solve(int i,string prev,string &w)
+    {
+        int n=w.size();
+        if(i==n && prev!=w)
+        {
+            return mpp[prev];
+        }
+        if(i==n)
+        {
+            return false;
+        }
+        string s=to_string(i)+" "+prev;
+        if(dp.find(s)!=dp.end())
+        {
+            return dp[s];
+        }
+        prev+=w[i];
+        if(mpp[prev] && prev!=w)
+        {
+            return dp[s]=solve(i+1,prev,w)||solve(i+1,"",w);
+        }
+        return dp[s]=solve(i+1,prev,w);
+    }
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        sort(words.begin(),words.end(),[&](string &w1,string w2){
+            return w1.size()<w2.size();
+        });
+        vector<string> ans;
+        for(auto it:words)
+        {
+            mpp[it]++;
+        }
+        int n=words.size();
+        for(int i=0;i<n;i++)
+        {
+            dp.clear();
+            if(solve(0,"",words[i]))
+            {
+                ans.push_back(words[i]);
+            }
+        }
+        return ans;
+    }
+};
