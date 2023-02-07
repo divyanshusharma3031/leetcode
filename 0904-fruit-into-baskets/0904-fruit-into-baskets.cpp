a972f14ae9275c3fbc576a4ac0f7d641ca438de1
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2023-02-07 10:38:46 +0530

    Time: 181 ms (73.00%), Space: 71.3 MB (48.83%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -3,13 +3,16 @@ public:
     int totalFruit(vector<int> &tree) {
         unordered_map<int, int> count;
         int i, j;
+        int mx=1;
         for (i = 0, j = 0; j < tree.size(); ++j) {
             count[tree[j]]++;
             if (count.size() > 2) {
                 if (--count[tree[i]] == 0)count.erase(tree[i]);
                 i++;
             }
+            mx=max(mx,j-i);
         }
-        return j - i;
+        mx=max(mx,j-i);
+        return mx;
     }
 };
