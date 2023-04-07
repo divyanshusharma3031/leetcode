616b29db5eaf2d01a78832af8c66451cf924c9a2
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2023-04-07 22:55:00 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (43.72%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
-  void combination(vector<vector<int>>& result, vector<int> sol, int k, int n) {
-    if (sol.size() == k && n == 0) { result.push_back(sol); return ; }
-    if (sol.size() < k) {
-      for (int i = sol.empty() ? 1 : sol.back() + 1; i <= 9; ++i) {
-        if (n - i < 0) break;
-        sol.push_back(i);
-        combination(result, sol, k, n - i);
-        sol.pop_back();
-      }
+    vector<vector<int>> ans;
+    void dfs(int i,vector<int> &nums,int target,vector<int> &temp,int k)
+    {
+        if(target<0)
+        {
+            return;
+        }
+        if(target==0)
+        {
+            if(temp.size()==k)
+            {
+                ans.push_back(temp);
+            }
+            return;
+        }
+        if(i==nums.size())
+        {
+            return;
+        }
+        for(int idx=i;idx<nums.size();idx++)
+        {
+            temp.push_back(nums[idx]);
+            dfs(idx+1,nums,target-nums[idx],temp,k);
+            temp.pop_back();
+        }
+        return;
     }
-  }
-
-  vector<vector<int>> combinationSum3(int k, int n) {
-    vector<vector<int>> result;
-    vector<int> sol;
-    combination(result, sol, k, n);
-    return result;
-  }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> nums;
+        for(int i=1;i<=9;i++)
+        {
+            nums.push_back(i);
+        }
+        vector<int> temp;
+        dfs(0,nums,n,temp,k);
+        return ans;
+    }
 };
