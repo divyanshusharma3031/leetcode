0d1be7d5e8c2007b130ad2b4b760f5ec201343f2
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2023-04-07 23:37:08 +0530

    Time: 14 ms (52.65%), Space: 12.6 MB (52.28%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    unordered_map<string,int> mpp;
+    int dp[301];
+    bool solve(int i,string &s)
+    {
+        int n=s.size();
+        if(i>=n)
+        {
+            return true;
+        }
+        if(dp[i]!=-1)
+        {
+            return dp[i];
+        }
+        string sub="";
+        bool b=false;
+        for(int idx=i;idx<n;idx++)
+        {
+            sub+=s[idx];
+            if(mpp[sub])
+            {
+                b=b|solve(idx+1,s);
+            }
+        }
+        return dp[i]=b;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        for(auto it:wordDict)
+        {
+            mpp[it]++;
+        }
+        memset(dp,-1,sizeof(dp));
+        return solve(0,s);
+    }
+};
