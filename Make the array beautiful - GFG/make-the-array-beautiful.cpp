e8e9f6e69b25224aeb24c349396f271f0a6c808e
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2023-04-08 11:49:31 +0530

    Added solution - LeetHub

---
+++ Make the array beautiful - GFG/make-the-array-beautiful.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+    private:
+          bool sign(int n)
+          {
+            return n >= 0;
+          }
+  public:
+    vector<int> makeBeautiful(vector<int> arr) {
+        // code here
+        set<int> s;
+        int n=arr.size();
+        for(int i=0;i<n;)
+        {
+            s.insert(i);
+            int j=i+1;
+            while(j<n && sign(arr[i])==sign(arr[j]))
+            {
+                s.insert(j);
+                j++;
+            }
+            while(j<n && sign(arr[i])!=sign(arr[j]) && s.size())
+            {
+                s.erase(--s.end());
+                j++;
+            }
+            i=j;
+        }
+        vector<int> ans;
+        for(auto it:s)
+        {
+            ans.push_back(arr[it]);
+        }
+        return ans;
+    }
+};
+
+// 7
+// -3 -1 -19 0 6 -13 12
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        Solution obj;
+        vector<int> res = obj.makeBeautiful(arr);
+        for (int i = 0; i < res.size(); i++) {
+            cout << res[i] << " ";
+        }
+
+        cout << "\n";
+    }
+}
+// } Driver Code Ends
