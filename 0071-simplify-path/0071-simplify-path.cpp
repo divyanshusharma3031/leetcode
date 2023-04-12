0134ace42b531b522db08624eb3b9a677997f8af
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2023-04-12 12:18:27 +0530

    Time: 9 ms (33.49%), Space: 7.8 MB (85.65%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,33 +1,5 @@
 class Solution {
 public:
-    string remove(string &str)
-    {
-        int n=str.size();
-        stack<int> s;
-        for(int i=0;i<n;i++)
-        {
-            if(str[i]=='/')
-            {
-                if(!s.empty() && s.top()=='/')
-                {
-                    continue;
-                }
-            }
-            s.push(str[i]);
-        }
-        string ans="";
-        while(!s.empty())
-        {
-            ans+=s.top();
-            s.pop();
-        }
-        reverse(ans.begin(),ans.end());
-        if(ans.back()=='/' && ans.size()!=1)
-        {
-            ans.pop_back();
-        }
-        return ans;
-    }
     string simplifyPath(string path) {
         path.push_back('/');
         int n=path.size();
@@ -44,18 +16,14 @@ public:
                     str="";
                     continue;
                 }
-                if(str=="..")
+                else if(str=="..")
                 {
-                    // cout<<" ";
                     if(!s.empty())
                         s.pop();
-                    str="";
-                    continue;
                 }
                 else
                 {
                     reverse(str.begin(),str.end());
-                    // cout<<str<<"\n";
                     s.push(str);
                 }
                 str="";
@@ -74,7 +42,10 @@ public:
         }
         ans="/"+ans;
         reverse(ans.begin(),ans.end());
-        // cout<<ans<<endl;
-        return remove(ans);
+        while(ans.size()>1 && ans.back()=='/')
+        {
+            ans.pop_back();
+        }
+        return ans;
     }
 };
