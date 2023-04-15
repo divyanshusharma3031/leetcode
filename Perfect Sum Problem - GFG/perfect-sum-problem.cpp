dd2a6d9aeabea98fd380a537e004169bc1e4b3de
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2023-04-15 17:46:08 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -6,66 +6,34 @@ using namespace std;
 
 const int mod=1e9+7;
 class Solution{
-    
+
 	public:
-	vector<vector<long long>> dp;
-	long long sz=0;
-	long long int power(long long x, long long y, int p=1e9+7)
-    {
-     
-        // Initialize answer
-        int res = 1;
-     
-        // Check till the number becomes zero
-        while (y > 0) {
-     
-            // If y is odd, multiply x with result
-            if (y % 2 == 1)
-                res = (res * x);
-     
-            // y = y/2
-            y = y >> 1;
-     
-            // Change x to x^2
-            x = (x * x);
+	int perfectSum(int arr[], int n, int sum)
+	{   int mod = 1000000007;
+        int dp[n+1][sum+1];
+        
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=sum;j++){
+                if(i==0)
+                {
+                    dp[i][j]=0;
+                }
+                if(j==0)
+                dp[i][j]=1;
+            }
         }
-        return res % p;
-    }
-	long long solve(int arr[],long long n,long long sum)
-	{
-	    if(n<=0)
-	    {
-	        return sum==0;
-	    }
-	    if(sum<0)
-	    {
-	        return 0;
-	    }
-	    if(dp[n][sum]!=-1)
-	    {
-	        return dp[n][sum];
-	    }
-	    if(sum==0)
-	    {
-	        long long x=0;
-	        for(long long j=0;j<sz;j++)
-	        {
-	            if(arr[j]==0)
-	            {
-	                x++;
-	            }
-	        }
-	        return dp[n][sum]=power(2,x);
-	    }
-	    return dp[n][sum]=((solve(arr,n-1,sum-arr[n-1]))%mod+solve(arr,n-1,sum)%mod)%mod;
-	}
-	long long perfectSum(int arr[], long long n, long long sum)
-	{
-        // Your code goes here
-        sz=n;
-        sort(arr,arr+n);
-        dp.resize(n+1,vector<long long>(sum+1,-1));
-        solve(arr,n,sum);
+        
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=sum;j++){
+                if(arr[i-1]<=j)
+                dp[i][j]=(dp[i-1][j-arr[i-1]]+dp[i-1][j])%mod;
+                else
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+        
+        
+        return dp[n][sum]%mod;
 	}
 	  
 };
