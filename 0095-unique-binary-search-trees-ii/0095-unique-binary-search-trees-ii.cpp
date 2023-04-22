1eaa55819800524270e6ddd2e63da37374341086
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2023-04-22 23:50:23 +0530

    Time: 23 ms (31.52%), Space: 15.6 MB (78.93%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    #define Node TreeNode
+    vector<TreeNode *> ans;
+    vector<TreeNode*> solve(int i,int j,vector<int> &arr)
+    {
+        if(i>j)
+        {
+            return {NULL};
+        }
+        // cout<<i<<" "<<j<<"\n";
+        if(i==j)
+        {
+            Node *root=new Node(arr[i]);
+            return {root};
+        }
+        vector<TreeNode*> v;
+        for(int k=i;k<=j;k++)
+        {   
+            vector<TreeNode *> temp1=solve(i,k-1,arr);
+            vector<TreeNode *> temp2=solve(k+1,j,arr);
+            for(auto it:temp1)
+            {
+                for(auto it2:temp2)
+                {
+                    Node *root=new Node(arr[k]);
+                    root->left=it;
+                    root->right=it2;
+                    v.push_back(root);
+                }
+            }
+        }
+        return v;
+    }
+    vector<TreeNode*> generateTrees(int n) {
+        vector<int> v;
+        for(int i=1;i<=n;i++)
+        {
+            v.push_back(i);
+        }
+        int i=0;
+        int j=n-1;
+        for(int k=i;k<=j;k++)
+        {
+            vector<TreeNode *> temp1=solve(i,k-1,v);
+            vector<TreeNode *> temp2=solve(k+1,j,v);
+            for(auto it:temp1)
+            {
+                for(auto it2:temp2)
+                {
+                    Node *root=new Node(v[k]);
+                    root->left=it;
+                    root->right=it2;
+                    ans.push_back(root);
+                }
+            }
+        }
+        return ans;
+    }
+};
