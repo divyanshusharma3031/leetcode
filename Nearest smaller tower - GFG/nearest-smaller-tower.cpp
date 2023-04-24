f2225b1f1923eab999f015faa3548157a8f27207
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2023-04-24 22:14:07 +0530

    Added solution - LeetHub

--- Nearest smaller tower - GFG/nearest-smaller-tower.cpp
+++ Nearest smaller tower - GFG/nearest-smaller-tower.cpp
@@ -11,16 +11,19 @@ using namespace std;
 
 class Solution
 {
-public:
-    vector<int> nearestSmallerTower(vector<int> arr)
+    typedef vector<pair<int,int>> vpi;
+    vpi Construct(vector<int> arr,bool reversed)
     {
-        // code here
         int n=arr.size();
-        vector<int> ans(n,0);
         stack<pair<int,int>> s;
-        vector<pair<int,int>> v1(n);
+        vpi v1(n);
         v1[0]={1e9,-1};
-        s.push({arr[0],0});
+        if(!reversed)
+            s.push({arr[0],0});
+        else
+        {
+            s.push({arr[0],n-1});
+        }
         for(int i=1;i<n;i++)
         {
             while(!s.empty() && s.top().first>=arr[i])
@@ -35,29 +38,25 @@ public:
             {
                 v1[i]=s.top();
             }
-            s.push({arr[i],i});
-        }
-        stack<pair<int,int>> s1;
-        reverse(arr.begin(),arr.end());
-        vector<pair<int,int>> v2(n);
-        v2[0]={1e9,-1};
-        s1.push({arr[0],n-1});
-        for(int i=1;i<n;i++)
-        {
-            while(!s1.empty() && s1.top().first>=arr[i])
-            {
-                s1.pop();
-            }
-            if(s1.empty())
-            {
-                v2[i]={1e9,-1};
-            }
+            if(!reversed)
+                s.push({arr[i],i});
             else
             {
-                v2[i]=s1.top();
+                s.push({arr[i],n-i-1});
             }
-            s1.push({arr[i],n-i-1});
         }
+        return v1;
+    }
+public:
+    
+    vector<int> nearestSmallerTower(vector<int> arr)
+    {
+        // code here
+        int n=arr.size();
+        vector<int> ans(n,0);
+        vpi v1=Construct(arr,false);
+        reverse(arr.begin(),arr.end());
+        vpi v2=Construct(arr,true);
         reverse(v2.begin(),v2.end());
         for(int i=0;i<n;i++)
         {
@@ -105,6 +104,9 @@ public:
         return ans;
     }
 };
+
+
+
 
 //{ Driver Code Starts.
 
