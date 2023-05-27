21d7d3dc5872465c48f67aa60696bd8ba624eafc
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2023-05-27 12:46:23 +0530

    Added solution - LeetHub

---
+++ Modify Linked List-1 - GFG/modify-linked-list1.cpp
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+struct Node* modifyTheList(struct Node *head);
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+void print(Node *head)
+{
+    Node *temp=head;
+	while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+
+// } Driver Code Ends
+/*Complete the function below
+Node is as follows:
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+class Solution{
+    public:
+    struct Node* modifyTheList(struct Node *head)
+    {
+        //add code here.
+        //with extra space this question is too easy
+        vector<int> v;
+        Node *p=head;
+        while(p)
+        {
+            v.push_back(p->data);
+            p=p->next;
+        }
+        for(int i=0;i<(v.size()/2);i++)
+        {
+             int temp=v[i];
+             v[i]=v[v.size()-i-1]-v[i];
+             v[v.size()-i-1]=temp;
+        }
+        p=head;
+        int i=0;
+        while(p)
+        {
+            p->data=v[i];
+            i++;
+            p=p->next;
+        }
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		struct Node *head = NULL,*temp=NULL;
+		while(n--){
+		    int a;
+            cin>>a;
+			Node *newNode = new Node(a);
+			if(head==NULL)
+                head=newNode;
+            else
+                temp->next=newNode;
+            temp=newNode;
+		}
+		Solution obj;
+		head = obj.modifyTheList(head);
+		print(head);
+		
+	}
+    return 0;
+}
+// } Driver Code Ends
