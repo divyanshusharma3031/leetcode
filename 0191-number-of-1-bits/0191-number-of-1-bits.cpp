60ab8db3ff9fbce46e869e070dabfb311a081306
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2023-05-28 17:59:12 +0530

    Time: 5 ms (29.67%), Space: 5.9 MB (74.41%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int rmsb=n&(-n);
-        int ans=0;
-        while(n>0)
-        {
-            n=n-rmsb;
-            ans++;
-            rmsb=n&(-n);
-        }
-        return ans;
+        return __builtin_popcount(n);
     }
 };
