8dca8d66e57e9399aea9b17ad3ad0599186bac3d
Divyanshu Katyan <89530118+divyanshusharma3031@users.noreply.github.com>
2023-06-02 17:31:26 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (15.58%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool isvalid(vector<int> arr,int n,int k,int mid)
+    {
+        int s=0;
+        int c=1;
+        for(int i=0;i<n;i++)
+        {
+            s+=arr[i];
+            if(s>mid)
+            {
+                c++;
+                s=arr[i];
+            }
+            if(c>k)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    int splitArray(vector<int>& weights, int days) {
+        int start=0;
+        int s=0;
+        int n=weights.size();
+        for(int i=0;i<n;i++)
+        {
+            start=max(start,weights[i]);
+            s+=weights[i];
+        }
+        int end=s;
+        int res=-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(isvalid(weights,n,days,mid))
+            {
+                res=mid;
+                end=mid-1;
+            }
+            else
+            {
+                start=mid+1;
+
+            }
+        }
+        return res;
+    }
+};
